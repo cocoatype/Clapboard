3d048d180535ba664e01527f2053ab5bdf241e91
Geoff Pado <geoff@pado.name>
2023-09-10 19:54:54 -0700

    Initial Commit

---
+++ Clapboard/ClapboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClapboardApp.swift
+//  Clapboard
+//
+//  Created by Geoff Pado on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ClapboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clapboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clapboard
+//
+//  Created by Geoff Pado on 9/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
